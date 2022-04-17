91061a802550b2714cfe92c38285bc3441f68df4
lbetterton <49688256+lbetterton@users.noreply.github.com>
2022-04-17 16:02:24 +0800

    Create random.py

---
+++ random.py
@@ -0,0 +1,14 @@
+import random
+ 
+# The limit for the extended ASCII Character set
+MAX_FLOOR = 97
+MAX_LIMIT = 122
+ 
+random_string = ''
+ 
+for _ in range(500):
+    random_integer = random.randint(MAX_FLOOR, MAX_LIMIT)
+    # Keep appending random characters using chr(x)
+    random_string += (chr(random_integer))
+ 
+print(random_string, len(random_string))
